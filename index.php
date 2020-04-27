b47f5289260eaf4bea50aed84103603a3b3f8e76
herihap <herithanx@gmail.com>
2020-04-27 10:47:49 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>try</title>
+</head>
+
+<body>
+    <h3> test remote </h3>
+    <ul>
+        <li>John Cena </li>
+        <li>Fernando </li>
+        <li>Muslera </li>
+    </ul>
+    <p>paragraph</p>
+
+
+</body>
+
+</html>
